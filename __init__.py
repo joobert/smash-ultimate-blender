089a7607c809a3b70be5016c6104c500ab56a6cf
Jacob <jacobgrasso10@gmail.com>
2026-09-11 20:08:50 -0400

    Bump version to 4.6.0

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ bl_info = {
     'category': 'Object',
     'location': 'View 3D > Tool Shelf > Ultimate',
     'description': 'A collection of tools for importing models and animations to smash ultimate.',
-    'version': (4, 5, 0),
+    'version': (4, 6, 0),
     'blender': (4, 4, 0),
     'warning': 'TO REMOVE: First "Disable" the plugin, then restart blender, then you can hit "Remove" to uninstall',
     'doc_url': 'https://github.com/ssbucarlos/smash-ultimate-blender/wiki',
